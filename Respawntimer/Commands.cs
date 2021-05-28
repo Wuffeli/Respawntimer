d48d9cbef87ac0811bf0e94260a83eedfed23593
yannt <yannt@DESKTOP-AFTFKG1>
2021-05-28 18:15:03 +0200

    Projektdateien hinzufügen.

---
+++ Respawntimer/Commands.cs
@@ -0,0 +1,27 @@
+﻿using Synapse.Command;
+using Synapse.Api;
+using System.Linq;
+using System;
+
+namespace Respawntimer
+{
+    [CommandInformation(
+        Name = "Respawntime",
+        Aliases = new string[] {"rt"},
+        Description = "Shows you the time until you respawn.",
+        Permission = "",
+        Platforms = new[] {Platform.ClientConsole, Platform.ServerConsole},
+        Usage = ".rt or .Respawntime"
+        )]
+    class Commands : ISynapseCommand
+    {
+        public CommandResult Execute(CommandContext context)
+        {
+            var respawntime = Round.Get.NextRespawn;
+            return new CommandResult
+            {
+                Message = $"You will respawn in {System.Math.Round(respawntime)}"
+            };
+        }
+    }
+}
---
+++ Respawntimer/pluginclass.cs
@@ -0,0 +1,31 @@
+﻿using Synapse.Api.Plugin;
+using Synapse.Api;
+using Synapse;
+using System;
+
+namespace Respawntimer
+{
+    [PluginInformation(
+        Author = "Wuffeli",
+        Description = "This will show, how long it will take until you respawn",
+        LoadPriority = 0,
+        Name = "Respawntimer",
+        SynapseMajor = 2,
+        SynapseMinor = 6,
+        SynapsePatch = 1,
+        Version = "V1.0.0"
+        )]
+    public class pluginclass : AbstractPlugin
+    {
+        public override void Load()
+        {
+            base.Load();
+            Server.Get.Events.Player.PlayerDeathEvent += Player_PlayerDeathEvent;
+        }
+        private void Player_PlayerDeathEvent(Synapse.Api.Events.SynapseEventArguments.PlayerDeathEventArgs ev)
+        {
+            var respawntime = Round.Get.NextRespawn;
+            ev.Victim.SendBroadcast(10, $"You will respawn in {System.Math.Round(respawntime)} seconds!");
+        }
+    }
+}
